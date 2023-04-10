243896f8d9c99253e34981591e88421f7ebb8727
kerasus <aliesmaeeli20@gmail.com>
2023-04-10 19:15:19 +0330

    -remove debugger -update version

--- ui/src/components/WidgetListCategory.vue
+++ ui/src/components/WidgetListCategory.vue
@@ -80,7 +80,6 @@ export default {
       return widgetCount === categoryCount
     },
     onSelectWidget (data) {
-      debugger
       this.$emit('selectWidget', data)
     },
     isCategoryItemWidget (categoryItemWidget) {
